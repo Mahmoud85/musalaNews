bee83e7ccc517d6ca10b8c7a8576e3c4f8bc1b78
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2021-12-11 23:20:33 +0200

    add api key as const

--- src/services/news/news.service.tsx
+++ src/services/news/news.service.tsx
@@ -1,9 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Constants from '../../utils/Constants';
 
 export default fetchNews = async () => {
   const lang = await AsyncStorage.getItem('lang');
-  const newsLang = lang=='bg'?'BG':'US'
+  const newsLang = lang=='bg'?'bg':'us'
+  const apiKey = Constants.APIKEY
     return fetch(
-      `https://newsapi.org/v2/top-headlines?country=${newsLang}&apiKey=495ff7a4c6de4b84b26ab5cd752d479e`
+      `https://newsapi.org/v2/top-headlines?country=${newsLang}&apiKey=${apiKey}`
     ).then(response => response.json());
   };
--- src/utils/Constants.tsx
+++ src/utils/Constants.tsx
@@ -3,5 +3,6 @@ export default {
  BULGARIAN: 'bg',
  LANGUAGEKEY: 'lang',
  ENGLANGLABEL:'English',
- BGLANGLABEL:'български'
+ BGLANGLABEL:'български',
+ APIKEY: '495ff7a4c6de4b84b26ab5cd752d479e'
 }
