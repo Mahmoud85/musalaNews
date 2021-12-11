dc6671f593dc34fef4fded5891089aa57a922d3e
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2021-12-11 22:00:31 +0200

    feat: initial project

---
+++ src/styles.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  rootContainer: theme =>(
+    {
+      flex: 1,
+      backgroundColor:theme.background
+    }
+  ) 
+});
