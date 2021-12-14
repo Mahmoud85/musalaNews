36f0e59c37a4b93c48ea2ce5ffad5d1b5760987e
mahmoud.bakhit@vodafone.com <mahmoud.bakhit@vodafone.com>
2021-12-14 21:45:36 +0200

    fix: prettier

--- src/models/NewsItem.tsx
+++ src/models/NewsItem.tsx
@@ -1,9 +1,9 @@
 export interface NewsItemModel {
-  title: string,
-  author: string,
-  publishedAt: string,
-  content: string,
-  urlToImage:string,
-  source: string,
-  description: string
-  }
+  title: string;
+  author: string;
+  publishedAt: string;
+  content: string;
+  urlToImage: string;
+  source: string;
+  description: string;
+}
